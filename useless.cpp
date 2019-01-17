69b7fee0db61f7d9cd7317380a5d7766f9437f75
Ricardo Taboada <taboada.19@dartmouth.edu>
2019-01-17 16:30:44 -0500

    Cleanup

--- useless.cpp
+++ useless.cpp
@@ -2,24 +2,38 @@
 #include <unistd.h>
 
 int count = 1;
-
 void printEllipses();
+void animateSeven();
 
 
 int main(int argc, char *argv[]) {
   while (true) {
-    std::cout << "ENTER me, baby";
+    std::cout << "ENTER";  
     std::cin.ignore();
     if (count == 3) {
       std::cout << "Slow down cowboy\n";
-      usleep(3000000);
+      usleep(2000000);
     }
     if (count % 7 == 0) {
-      std::cout << "Girl you gotta love your man\n";
-      usleep(2000000);
+      std::cout << "STOP\n";
+      usleep(1000000);
+      animateSeven();
     }
     printEllipses();
     count++;
+  }
+}
+
+void animateSeven()
+{
+  std::cout << "456763 + 7869231?\n";
+  std::string answer;
+  std::cin >> answer;
+  if (answer == "YES") {
+    std::cout << "y";
+  }
+  else {
+    std::cout << "NNNNNNNNN";
   }
 }
 
